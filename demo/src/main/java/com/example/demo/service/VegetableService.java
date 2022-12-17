851eff17ddf0602dcd11e263932ffb9e8eb0252d
LAPTOP-N8E0I7QK\gotou <gotoukei57@gmail.com>
2022-12-17 22:48:20 +0900

    2022/12/17 夜の部終了

---
+++ demo/src/main/java/com/example/demo/service/VegetableService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Vegetable;
+import com.example.demo.repository.VegetableRepository;
+
+@Service
+public class VegetableService {
+
+    @Autowired
+    private VegetableRepository vegetableRepository;
+
+    // 空の場合はcontrollerでstatuscode決める際にやってやればいい(sutatuscodeを決めるのだからそのためのロジックがあるのは自然)
+    // TODO: ここではinternalserverエラーのような具体的でないエラーをThrowするだけでよさそう
+    public List<Vegetable> getAll(String name) {
+        return null;
+    }
+
+    // idのデータあるかチェックする(Optionalに入れる)、ない場合はnull入れてcontrollerでNotFoundExcetionで、それ以外ならThrow投げる
+    public Vegetable update(long id, Vegetable vegetable) {
+        return null;
+    }
+}
